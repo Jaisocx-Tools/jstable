d6c7c56abdfcfa84f0e38c055df22261d02b4a50
jaisocx <illia.polianskyi@protonmail.com>
2024-09-28 05:40:06 +0200

    feature: order by

--- www/js/JSTable.js
+++ www/js/JSTable.js
@@ -1,127 +1,130 @@
 class JSTable {
-  constructor(htmlElementId, dataUrl, tableHeader) {
+  constructor() {
     this.MAX_WIDTH_MOBILE_LOOK_AND_FEEL = 830;
 
-    this.htmlElementId = htmlElementId;
-    this.dataUrl = dataUrl;
-    this.tableHeader = tableHeader;
+    this.htmlElementId = '';
+    this.metadataUrl = '';
+    this.dataUrl = '';
 
-    this.htmlElement = document.getElementById(this.htmlElementId);
+    this.tableMetadata = null;
+    this.loadedTableData = null;
+    this.tableData = null;
 
-    this.assignEventsHandlres();
+    this.htmlElement = null;
+
+    this.debug = true;
+
+    this.orderByField = '';
+    this.orderByOrder = 'ASC';
   }
 
-  assignEventsHandlres() {
+  setElementId(id) {
+    this.htmlElementId = id;
+    this.htmlElement = document.getElementById(this.htmlElementId);
+    this.assignEventsHandlers();
+
+    return this;
+  }
+
+  setMetadataUrl(url) {
+    this.metadataUrl = url;
+    return this;
+  }
+
+  setDataUrl(url) {
+    this.dataUrl = url;
+    return this;
+  }
+
+  setMobileLookAndFeelMaxWidth(width) {
+    this.MAX_WIDTH_MOBILE_LOOK_AND_FEEL = width;
+    return this;
+  }
+
+  assignEventsHandlers() {
     window.addEventListener('resize', () => {
-      this.applyColumnsWidths(this.tableHeader);
+      this.applyColumnsWidths();
       this.adjustHeaderPaddingRight();
+    });
+
+    this.htmlElement.addEventListener('click', event => {
+      const elem = event.target;
+      const parentRowLabel = elem.closest('.jstable-row-field');
+      if (!parentRowLabel || !elem.classList.contains('label')) {
+        return;
+      }
+
+      event.preventDefault();
+
+      this.orderByToggle(elem.dataset.fieldname);
     });
   }
 
-  adjustHeaderPaddingRight() {
-    const tableBody = document.querySelector('#' + this.htmlElementId + ' .jstable-body');
-    const scrollbarWidth = this.getScrollbarWidth(tableBody);
-    const rowHeader = document.querySelector('#' + this.htmlElementId + ' .jstable-row.header');
-    rowHeader.style.paddingRight = scrollbarWidth + 'px';
-  }
-
-  applyColumnsWidths(tableHeader) {
-    let gridTemplateColumnsValue = '100%';
-
-    if (!window.matchMedia(`(max-width: ${this.MAX_WIDTH_MOBILE_LOOK_AND_FEEL}px)`).matches) {
-      gridTemplateColumnsValue = this.generateGridTemplateColumnsStyle(tableHeader);
-    }
-
-    let width = this.htmlElement.clientWidth;
-    width = Math.min(document.body.clientWidth, width);
-
-    console.log("width", width);
-
-    const mediaStylesheetLittle = document.getElementById('jstableStylesheetLittle');
-
-    if (width > this.MAX_WIDTH_MOBILE_LOOK_AND_FEEL) {
-      gridTemplateColumnsValue = this.generateGridTemplateColumnsStyle(tableHeader);
-      mediaStylesheetLittle.disabled = true;
-    } else {
-      mediaStylesheetLittle.disabled = false;
-    }
-
-    const tableHeadHtmlElement = document.querySelector(`#${this.htmlElementId} .jstable-row.header`);
-    tableHeadHtmlElement.style.gridTemplateColumns = gridTemplateColumnsValue;
-
-    const tableRowsHtmlElementsCollection = document.querySelectorAll(`#${this.htmlElementId} .jstable-body .jstable-row`);
-    let rowHtmlElement = null;
-    for (let i=0; i < tableRowsHtmlElementsCollection.length; i++) {
-      rowHtmlElement = tableRowsHtmlElementsCollection.item(i);
-      rowHtmlElement.style.gridTemplateColumns = gridTemplateColumnsValue;
-    }
-  }
-
-  generateGridTemplateColumnsStyle(tableHeader) {
-    let gridTemplateColumns = '3rem ';
-
-    for (let fieldName in tableHeader) {
-      let fieldMetadata = tableHeader[fieldName];
-      gridTemplateColumns += fieldMetadata.width + ' ';
-    }
-
-    return gridTemplateColumns;
-  }
-
-  loadJsonData() {
-    fetch(this.dataUrl)
-      .then(response => response.json())
+  load() {
+    fetch(this.metadataUrl)
+      .then(responseMetadata => responseMetadata.json())
+      .then(tableMetadata => {
+        this.tableMetadata = tableMetadata;
+        return fetch(this.dataUrl);
+      })
+      .then(responseTableData => responseTableData.json())
       .then(
-        json => {
-          this.renderTable(json);
-          this.applyColumnsWidths(this.tableHeader);
-          this.adjustHeaderPaddingRight();
+        tableDataJson => {
+          this.loadedTableData = tableDataJson;
+          this.tableData = [...tableDataJson];
+          this.renderTableAndAdjustSizes();
         }
       );
   }
 
-  renderTable(json) {
+  renderTableAndAdjustSizes() {
+    this.renderTable();
+    this.applyColumnsWidths();
+    this.adjustHeaderPaddingRight();
+  }
+
+  renderTable() {
     this.htmlElement.innerHTML = '';
 
-    let headerHtml = this.renderHeader(this.tableHeader);
+    let headerHtml = this.renderHeader();
 
     let rowCounter = 0;
     let tableRowsHtml = '<div class="jstable-body">';
-    for (let record of json) {
-      tableRowsHtml += this.renderRecord(record, rowCounter, this.tableHeader);
+    for (let record of this.tableData) {
+      tableRowsHtml += this.renderRecord(record, rowCounter);
       rowCounter++;
     }
     tableRowsHtml += '</div>';
 
     this.htmlElement.innerHTML = headerHtml + tableRowsHtml;
+
+    this.addEventListenersOrderBy();
   }
 
-  renderHeader(tableHeader) {
+  addEventListenersOrderBy() {
+    const tableHeaderCells = document.querySelectorAll(`#${this.htmlElementId} .jstable-row.header .jstable-row-field`);
+    tableHeaderCells
+      .forEach(cell => {
+        cell.addEventListener('click', () => {
+          this.orderByToggle(cell.dataset.fieldname);
+        });
+      });
+  }
+
+  renderHeader() {
     let headerHtml = '<div class="jstable-row header">';
     headerHtml += '<div class="jstable-row-field counter"><span> </span></div>';
 
-    for (let fieldName in tableHeader) {
-      let fieldMetadata = tableHeader[fieldName];
-      headerHtml += `<div class="jstable-row-field"><span> ${fieldMetadata.title} </span></div>`;
+    for (let fieldName in this.tableMetadata) {
+      let fieldMetadata = this.tableMetadata[fieldName];
+      headerHtml += `<div class="jstable-row-field" data-fieldname="${fieldName}"><span> ${fieldMetadata.title} </span></div>`;
     }
 
     headerHtml += '</div>';
     return headerHtml;
-
-    /*return `
-      <div class="jstable-row header">
-        <div class="jstable-row-field counter"><span> </span></div>
-        <div class="jstable-row-field right"><span> ${tableHeader.id} </span></div>
-        <div class="jstable-row-field"><span> ${tableHeader.title} </span></div>
-        <div class="jstable-row-field"><span> ${tableHeader.description} </span></div>
-        <div class="jstable-row-field"><span> ${tableHeader.image_src} </span></div>
-        <div class="jstable-row-field"><span> ${tableHeader.author} </span></div>
-        <div class="jstable-row-field"><span> ${tableHeader.updated_at} </span></div>
-      </div>
-    `;*/
   }
 
-  renderRecord(record, rowCounter, tableHeader) {
+  renderRecord(record, rowCounter) {
     let rowHtml = '<div class="jstable-row">';
 
     rowHtml += `<div class="jstable-row-field counter right">
@@ -129,8 +132,8 @@ class JSTable {
           <span class="text"> ${rowCounter + 1} </span>
         </div>`;
 
-    for (let fieldName in tableHeader) {
-      const fieldMetadata = tableHeader[fieldName];
+    for (let fieldName in this.tableMetadata) {
+      const fieldMetadata = this.tableMetadata[fieldName];
       
       let align = fieldMetadata.align;
       if (!align) {
@@ -147,48 +150,61 @@ class JSTable {
       }
 
       rowHtml += `<div class="jstable-row-field ${align}">
-          <span class="label"> ${fieldMetadata.title} </span>
+          <span class="label" data-fieldname="${fieldName}"> ${fieldMetadata.title} </span>
           <span class="text"> ${valueHtml} </span>
         </div>`;
     }
   
     rowHtml += '</div>';
     return rowHtml;
+  }
 
-    /*return `
-      <div class="jstable-row">
-        <div class="jstable-row-field counter right">
-          <span class="label"> </span>
-          <span class="text"> ${rowCounter + 1} </span>
-        </div>
-        <div class="jstable-row-field right">
-          <span class="label"> ${tableHeader.id} </span>
-          <span class="text"> ${record.id} </span>
-        </div>
-        <div class="jstable-row-field">
-          <span class="label"> ${tableHeader.title} </span>
-          <span class="text"> ${record.title} </span>
-        </div>
-        <div class="jstable-row-field">
-          <span class="label"> ${tableHeader.description} </span>
-          <span class="text"> ${record.description} </span>
-        </div>
-        <div class="jstable-row-field image">
-          <span class="label"> ${tableHeader.image_src} </span>
-          <span class="text">
-            <img src="${record.image_src}" alt="image"/>
-          </span>
-        </div>
-        <div class="jstable-row-field">
-          <span class="label"> ${tableHeader.author} </span>
-          <span class="text"> ${record.author} </span>
-        </div>
-        <div class="jstable-row-field">
-          <span class="label"> ${tableHeader.updated_at} </span>
-          <span class="text"> ${record.updated_at} </span>
-        </div>
-      </div>
-    `;*/
+  applyColumnsWidths() {
+    let gridTemplateColumnsValue = '100%';
+
+    if (!window.matchMedia(`(max-width: ${this.MAX_WIDTH_MOBILE_LOOK_AND_FEEL}px)`).matches) {
+      gridTemplateColumnsValue = this.generateGridTemplateColumnsStyle(this.tableMetadata);
+    }
+
+    let width = this.htmlElement.clientWidth;
+    width = Math.min(document.body.clientWidth, width);
+
+    const mediaStylesheetLittle = document.getElementById('jstableStylesheetLittle');
+
+    if (width > this.MAX_WIDTH_MOBILE_LOOK_AND_FEEL) {
+      gridTemplateColumnsValue = this.generateGridTemplateColumnsStyle(this.tableMetadata);
+      mediaStylesheetLittle.disabled = true;
+    } else {
+      mediaStylesheetLittle.disabled = false;
+    }
+
+    const tableHeadHtmlElement = document.querySelector(`#${this.htmlElementId} .jstable-row.header`);
+    tableHeadHtmlElement.style.gridTemplateColumns = gridTemplateColumnsValue;
+
+    const tableRowsHtmlElementsCollection = document.querySelectorAll(`#${this.htmlElementId} .jstable-body .jstable-row`);
+    let rowHtmlElement = null;
+    for (let i=0; i < tableRowsHtmlElementsCollection.length; i++) {
+      rowHtmlElement = tableRowsHtmlElementsCollection.item(i);
+      rowHtmlElement.style.gridTemplateColumns = gridTemplateColumnsValue;
+    }
+  }
+
+  generateGridTemplateColumnsStyle() {
+    let gridTemplateColumns = '3rem ';
+
+    for (let fieldName in this.tableMetadata) {
+      let fieldMetadata = this.tableMetadata[fieldName];
+      gridTemplateColumns += fieldMetadata.width + ' ';
+    }
+
+    return gridTemplateColumns;
+  }
+
+  adjustHeaderPaddingRight() {
+    const tableBody = document.querySelector('#' + this.htmlElementId + ' .jstable-body');
+    const scrollbarWidth = this.getScrollbarWidth(tableBody);
+    const rowHeader = document.querySelector('#' + this.htmlElementId + ' .jstable-row.header');
+    rowHeader.style.paddingRight = scrollbarWidth + 'px';
   }
 
   getScrollbarWidth(element) {
@@ -199,4 +215,41 @@ class JSTable {
     // The scrollbar width is the difference
     return totalWidth - clientWidth;
   }
+
+  orderByToggle(field) {
+    let order = 'ASC';
+    if (this.orderByField === field) {
+      order = (this.orderByOrder === 'ASC') ? 'DESC' : 'ASC';
+    }
+    this.orderBy(field, order);
+  }
+
+  orderBy(field, order) {
+    if (!field) {
+      return;
+    }
+
+    this.orderByField = field;
+    this.orderByOrder = order;
+
+    const numOrder = (order === 'ASC') ? 1 : (-1);
+
+    this.tableData = [
+      ...this.loadedTableData
+        .sort((a, b) => {
+          const fieldValueA = a[field];
+          const fieldValueB = b[field];
+          if (fieldValueA > fieldValueB) {
+            return numOrder;
+          } else if (fieldValueA < fieldValueB) {
+            return -numOrder;
+          } else {
+            return 0;
+          }
+        })
+    ];
+
+    this.renderTableAndAdjustSizes();
+  }
 }
+
